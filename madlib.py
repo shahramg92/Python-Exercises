27359300cc054dfb681b87fe91fc2f9a0fb285ef
Shahram Ghassemi <shahramg92@hotmail.com>
2018-05-04 17:13:31 -0500

    update

---
+++ madlib.py
@@ -0,0 +1,5 @@
+print("Please fill in the blanks below:");
+print("___name___'s favorite subject in school is ___subject___");
+name = input("What is your name? ");
+subject = input("What is the subject? ");
+print("{}'s favorite subject in school is {}".format(name, subject))
